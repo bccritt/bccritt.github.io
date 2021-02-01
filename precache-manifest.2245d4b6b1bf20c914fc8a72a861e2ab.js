a3c07147753ed0a4ee120fb0a98530aabdaef1a2
unknown <andrew.zavatin@yahoo.com>
2021-02-01 11:52:44 +0200

    start

---
+++ precache-manifest.2245d4b6b1bf20c914fc8a72a861e2ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f615172fe5e4879d375231caa4aa94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f64bbab75b95b515d2a",
+    "url": "/static/css/main.769bd808.chunk.css"
+  },
+  {
+    "revision": "998ec7b777570b478167",
+    "url": "/static/js/2.804ded2c.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.804ded2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f64bbab75b95b515d2a",
+    "url": "/static/js/main.46431ee2.chunk.js"
+  },
+  {
+    "revision": "9933ea9142b6712fefc1",
+    "url": "/static/js/runtime-main.cb9c7104.js"
+  },
+  {
+    "revision": "21154dbed9dfa8c48d7f54f4e9d246b1",
+    "url": "/static/media/arrow2.21154dbe.svg"
+  },
+  {
+    "revision": "66874b36f1907b3e384783833dc59629",
+    "url": "/static/media/bcr-ro.66874b36.PNG"
+  },
+  {
+    "revision": "5d1bbc15c4a109aab3173bd8885c6cc9",
+    "url": "/static/media/bg-img.5d1bbc15.jpg"
+  },
+  {
+    "revision": "43c91517111da84ce4771e886649fea1",
+    "url": "/static/media/eng.43c91517.PNG"
+  },
+  {
+    "revision": "347ec373a272269bec3527280f1bfb63",
+    "url": "/static/media/ro.347ec373.PNG"
+  }
+]);
